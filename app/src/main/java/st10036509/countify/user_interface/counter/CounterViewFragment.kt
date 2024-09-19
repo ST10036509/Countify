0fd1b431e2ab0c200b0aa0bf73454864d34cf54c
Leonard <st10026396@vcconnect.edu.za>
2024-09-19 23:45:59 +0200

    OF-73: Added item object, adjusted fragments, renamed a drawable.

--- app/src/main/java/st10036509/countify/user_interface/counter/CounterViewFragment.kt
+++ app/src/main/java/st10036509/countify/user_interface/counter/CounterViewFragment.kt
@@ -1,4 +1,30 @@
 package st10036509.countify.user_interface.counter
 
-class CounterViewFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import st10036509.countify.R
+import st10036509.countify.service.NavigationService
+import st10036509.countify.user_interface.account.RegisterFragment
+
+class CounterViewFragment : Fragment()
+{
+    // on fragment creation inflate the fragment
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_counter_view, container, false)
+    }
+
+    // when the view is created bind the register TextView to its object
+    // reference and handle oncClick event
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
