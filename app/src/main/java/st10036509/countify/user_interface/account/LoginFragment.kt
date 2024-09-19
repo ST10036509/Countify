ff165c8434fe37f49cb053b3120590d4cfa78c45
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-09-19 22:19:28 +0200

    Navigation Service Implemented

--- app/src/main/java/st10036509/countify/user_interface/account/LoginFragment.kt
+++ app/src/main/java/st10036509/countify/user_interface/account/LoginFragment.kt
@@ -4,32 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import st10036509.countify.R
+import st10036509.countify.service.NavigationService
 
 class LoginFragment : Fragment() {
 
+    // create object reference for components to handle events
+    private lateinit var registerButton: TextView
+
+    // on fragment creation inflate the fragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    // when the view is created bind the register TextView to its object
+    // reference and handle oncClick event
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val emailEditText: EditText = view.findViewById(R.id.editTextEmail)
-        val passwordEditText: EditText = view.findViewById(R.id.editTextPassword)
-        val loginButton: Button = view.findViewById(R.id.buttonLogin)
-
-        loginButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-            // Handle login logic
+        //bind reference object to fragment xml component
+        registerButton = view.findViewById(R.id.createAccountButton)
+        //handle onClick event
+        registerButton.setOnClickListener {
+            // use the navigation service (singleton) to navigate to the RegisterFragment
+            NavigationService.navigateToFragment(RegisterFragment(), R.id.fragment_container)
         }
-    } */
+
+    }
 }
