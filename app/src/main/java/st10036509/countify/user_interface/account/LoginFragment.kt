43030c56240f77ec840c4acda5c09886cbb3943d
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-09-20 12:19:41 +0200

    Basic Email & Password Login Implemented

--- app/src/main/java/st10036509/countify/user_interface/account/LoginFragment.kt
+++ app/src/main/java/st10036509/countify/user_interface/account/LoginFragment.kt
@@ -4,19 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import st10036509.countify.R
+import st10036509.countify.service.FirebaseAuthService
 import st10036509.countify.service.NavigationService
+import st10036509.countify.service.Toaster
 import st10036509.countify.user_interface.counter.CounterViewFragment
 
 class LoginFragment : Fragment() {
 
+    // setup service instances
+    private lateinit var toaster: Toaster // handle toasting message
+
     // create object reference for components to handle events
     private lateinit var registerButton: TextView
-
     private lateinit var loginButton: CardView
+    private lateinit var  googleSSOButton: CardView
 
     // on fragment creation inflate the fragment
     override fun onCreateView(
@@ -31,19 +37,68 @@ class LoginFragment : Fragment() {
     // reference and handle oncClick event
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loginButton = view.findViewById(R.id.loginButton)
 
-        loginButton.setOnClickListener{
+        // get the current user (if they are logged in)
+        val currentUser = FirebaseAuthService.getCurrentUser()
+
+        // check if a user account is logged in and navigate to the home page
+        if (currentUser != null) {
             NavigationService.navigateToFragment(CounterViewFragment(), R.id.fragment_container)
+            return
         }
 
-        //bind reference object to fragment xml component
-        registerButton = view.findViewById(R.id.createAccountButton)
+        // initialise UI components
+        setupUIComponents(view)
+    }
+
+    private fun setupUIComponents(view: View) {
+        // bind reference objects to fragment xml components
+        // ACTION BUTTONS:
+        loginButton = view.findViewById(R.id.btn_Login)
+        registerButton = view.findViewById(R.id.btn_Register)
+        googleSSOButton = view.findViewById(R.id.btn_SSO)
+        //INPUTS:
+        val emailEditText = view.findViewById<EditText>(R.id.edt_Email)
+        val passwordEditText = view.findViewById<EditText>(R.id.edt_Password)
+        //services
+        toaster =  Toaster(this)
+
+        // handle onClick event
+        loginButton.setOnClickListener{
+
+            // get input values
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+
+            //check if inputs are valid
+            if (!areInputsValid(email, password)) { // inputs are invalid
+                toaster.showToast(getString(R.string.login_invalid_inputs))
+            } else { // inputs are valid
+                handleEmailPasswordLogin(email, password) // verify login credentials an handle success/failure
+            }
+        }
+
         //handle onClick event
         registerButton.setOnClickListener {
             // use the navigation service (singleton) to navigate to the RegisterFragment
             NavigationService.navigateToFragment(RegisterFragment(), R.id.fragment_container)
         }
+    }
 
+    // method to check if email and password are valid inputs
+    private fun areInputsValid(email: String, password: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty()
+    }
+
+    // method to handle email & password login authentication
+    private fun handleEmailPasswordLogin(email: String, password: String) {
+        FirebaseAuthService.loginUser(email, password) { isSuccess, errorMessage ->
+            if (isSuccess) { // login successful
+                NavigationService.navigateToFragment(CounterViewFragment(), R.id.fragment_container)
+                toaster.showToast(getString(R.string.login_successul))
+            } else {
+                toaster.showToast(errorMessage) // failed login
+            }
+        }
     }
 }
