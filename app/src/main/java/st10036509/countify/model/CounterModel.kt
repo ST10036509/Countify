97d296e006a98898f59aac4d135d18fa80e7fc50
Kurt Bester <kurt.bester@liberty.co.za>
2024-09-23 11:36:38 +0200

    OF-84 User can now push to database by creating a new counter. Adjusted rules in firestore.

--- app/src/main/java/st10036509/countify/model/CounterModel.kt
+++ app/src/main/java/st10036509/countify/model/CounterModel.kt
@@ -1,10 +1,14 @@
 package st10036509.countify.model
 
+import com.google.firebase.auth.FirebaseAuth
+import st10036509.countify.service.FirestoreService
+
 data class CounterModel(
-    val counterId: String = "", // counter uid
+    val userId: String = "",
     var name: String = "",
     var startValue: Int = 0, // initial value of the counter
     var changeValue: Int = 1, // the value which the counter will change by on inc/dec
     var repetition: String = "none", // can be daily, weekly, monthly, yearly, or none
-    val createdTimestamp: Long = System.currentTimeMillis() // date the counter was created (handle the notifications)
+    val createdTimestamp: Long = System.currentTimeMillis(), // date the counter was created (handle the notifications)
+    var currentValue: Int = 0 // current value of the counter with default value
 )
