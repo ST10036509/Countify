37232578e94c235f5d685b2bc58a629dbb19ee40
Cameron Brooks <cbrooks02work@gmail.com>
2024-09-24 13:30:17 +0200

    random advice API setup

---
+++ app/src/main/java/st10036509/countify/AdviceApiService.kt
@@ -0,0 +1,10 @@
+package st10036509.countify
+
+import retrofit2.Call
+import retrofit2.http.GET
+import st10036509.countify.model.AdviceResponse
+
+interface AdviceApiService {
+    @GET("advice")
+    fun getRandomAdvice(): Call<AdviceResponse>
+}
