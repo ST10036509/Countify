b495a04e620f0c743e9d6077b96f83ee3d56d8a4
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-11-02 20:49:03 +0200

    updated main activity

--- app/src/main/java/st10036509/countify/MainActivity.kt
+++ app/src/main/java/st10036509/countify/MainActivity.kt
@@ -7,15 +7,19 @@ Last Modified: 25/09/2024
 
 package st10036509.countify
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.firebase.Firebase
 import com.google.firebase.messaging.messaging
 import st10036509.countify.service.NavigationService
 import st10036509.countify.user_interface.account.LoginFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_NOTIFICATION_PERMISSION = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +29,14 @@ class MainActivity : AppCompatActivity() {
 
         Firebase.messaging.isAutoInitEnabled = true
 
-        /*
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
                 REQUEST_NOTIFICATION_PERMISSION
             )
-        }*/
+        }
 
         // initialise singleton navigation service upon application start and provide the Fragment Manager
         NavigationService.initialise(supportFragmentManager)
